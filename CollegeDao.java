77a3a2087f9fbbcfbb8a9f4202fc0626c6e3d74a
devikasri8 <39004555+devikasri8@users.noreply.github.com>
2020-05-10 11:44:54 +0530

    Update CollegeDao.java

--- CollegeDao.java
+++ CollegeDao.java
@@ -5,19 +5,15 @@ import com.globalStore.Data;
 import com.user.Branch;
 
 public class CollegeDao {
-	public Data addClg = new Data();
+	public Data addClg;
 	public void addCollege(College college) {
-		addClg.storeClgDetails(college);
+		addClg.clgDetails.put(college.getClgId(), college);
 		System.out.println(addClg.clgDetails.size());
 	}
-	
-	public College getThreshold(String clgId) {
-		return addClg.clgDetails.get(clgId);
-		//System.out.println(addClg.clgDetails.get(clgId).branches.toString());
-		//System.out.println(addClg.clgDetails.size());
-	}
+
 	public void printAllColleges() {
 		addClg.clgDetails.forEach((k,v) -> System.out.println(v.toString()));
 		System.out.println(addClg.clgDetails.size());
 	}
 }
+
