1ec1a79f4aa3365af971de161d5bfd91c706ad4f
Pravallika Boppana <38985461+pravallika-boppana@users.noreply.github.com>
2020-05-11 18:11:57 +0530

    Update UserInteractionService.java

--- user/UserInteractionService.java
+++ user/UserInteractionService.java
@@ -20,35 +20,30 @@ public class UserInteractionService {
 		// TODO Auto-generated method stub
 		String line = "";  
 		String splitBy = ",";  
-		CollegeDao totalCollegeDetails = new CollegeDao();
-		College college = new College();
-		Student student = new Student();
-		CounsellingService counselling = new CounsellingService();
-		AllotmentService allotment = new AllotmentService();
 		try   
 		{  
 		//parsing a CSV file into BufferedReader class constructor  
-		BufferedReader br = new BufferedReader(new FileReader("C:\\\\Users\\\\user\\\\Desktop\\\\acms project\\\\Counselling\\\\src\\\\student.csv"));  
-		StudentDao s = new StudentDao();
+		BufferedReader br = new BufferedReader(new FileReader("//home//pravallika//Desktop//counsellingProject//student.csv"));  
+		StudentDao studentDao = new StudentDao();
 		while ((line = br.readLine()) != null)   //returns a Boolean value  
 		{  
 		String[] studentDetails = line.split(splitBy);    // use comma as separator  
-		
-		student = new Student(studentDetails[0], Long.parseLong(studentDetails[1]), Long.parseLong(studentDetails[2]));
-		s.addStudent(student);
+		Student student = new Student(studentDetails[0], Long.parseLong(studentDetails[1]), Long.parseLong(studentDetails[2]));
+		studentDao.addStudent(student);
 		}
 		//s.getAllStudents();
 		line = "";
 		
-		br = new BufferedReader(new FileReader("C:\\Users\\user\\Desktop\\acms project\\Counselling\\src\\college.csv"));
+		br = new BufferedReader(new FileReader("//home//pravallika//Desktop//counsellingProject//college.csv"));
 		while((line = br.readLine()) != null) {
 		  String[] collegeDetails = line.split(splitBy);
-		  college = new College(collegeDetails[0], collegeDetails[1], Integer.parseInt(collegeDetails[2]));
+		  College college = new College(collegeDetails[0], collegeDetails[1], Integer.parseInt(collegeDetails[2]));
 		  for(int i = 3; i < collegeDetails.length; i += 3) {
 			  Branch branch = new Branch(collegeDetails[i], Integer.parseInt(collegeDetails[i+1]), Integer.parseInt(collegeDetails[i+2]));
 			  college.addBranch(branch);
 		   }
-		  totalCollegeDetails.addCollege(college);
+		  CollegeDao collegeDao = new CollegeDao();
+		  collegeDao.addCollege(college);
 		}
 		//totalCollegeDetails.printAllColleges();
 		}
@@ -59,6 +54,10 @@ public class UserInteractionService {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int choice;
 		int x;
+		CounsellingService counsellingService = new CounsellingService();
+		AllotmentService allotmentService = new AllotmentService();
+		CollegeDao collegeDao = new CollegeDao();
+		StudentDao studentDao = new StudentDao();
 		while(true) {
 			System.out.println("Enter your choice:");
 			System.out.println("1.Request seat allocation");
@@ -72,49 +71,49 @@ public class UserInteractionService {
 				case 1: System.out.println("Enter Student Id");
 						long stuId = Long.parseLong(br.readLine());
 						System.out.println("Enter Student preferences as college Id and Branch Id");
-						ArrayList<String[]> pref = new ArrayList<String[]>();
+						Preference[] preferences = new Preference[3];
 						for(int i = 0; i < 3; i++) {
-							String[] preferences = br.readLine().split(" ");
-							pref.add(preferences);
+							String[] preference = br.readLine().split(" ");
+							preferences[i] = new Preference(preference[0], preference[1]);
 						}
-						RequestedAllotment ra = counselling.addPreferences(stuId, pref);
-						allotment.allotClg(ra);
-						break;
-				case 2:System.out.println("How many top desirable colleges you want");
-						x = Integer.parseInt(br.readLine());
-						if(checkRange(x)) {
+						RequestedAllotment requestAllotment = counsellingService.addPreferences(stuId, preferences);
 						
-						System.out.println("Top " + x +" Desirable colleges");
-						counselling.topDesirable(x);
+						allotmentService.allotClg(requestAllotment);
+						break;
+				case 2:System.out.println("How many top desirable colleges you want?(select a number < 20)");
+						x = Integer.parseInt(br.readLine());
+						if(checkRange(x)) {						
+     					    System.out.println("Top " + x +" Desirable colleges");
+						    counsellingService.topDesirable(x);
 						}
 						else
-							System.out.println("Data not available");
+							System.out.println("Invalid number");
 						break;
-				case 3:System.out.println("How many top filled colleges you want");
+				case 3:System.out.println("How many top filled colleges you want?(select a number < 20)");
 						x = Integer.parseInt(br.readLine());
 						if(checkRange(x)) {
 							System.out.println("Top " + x +" filled colleges");
-							counselling.topFilled(x);
+							counsellingService.topFilled(x);
 						}
 						else
-							System.out.println("Data not available");
+							System.out.println("Invalid number");
 						break;
-				case 4:System.out.println("How many top unfilled colleges you want");
+				case 4:System.out.println("How many top unfilled colleges you want?(select a number < 20)");
 						x = Integer.parseInt(br.readLine());
 						if(checkRange(x)) {
 							System.out.println("Top " + x +" unfilled colleges");
-							counselling.topUnfilled(x);
+							counsellingService.topUnfilled(x);
 						}
 						else
-							System.out.println("Data not available");
+							System.out.println("Invalid number");
 						break;
 				case 5:System.out.println("Enter student Id");
 						long stdId = Long.parseLong(br.readLine());
-						System.out.println(student.getAllotedCollege(stdId));
+						System.out.println(counsellingService.getAllotedCollege(stdId));
 						break;
 				case 6:System.out.println("Enter college id");
 						String clgId = br.readLine();
-						college.getThreshold(clgId);
+						collegeDao.getThreshold(clgId);
 						break;	
 				default:System.out.println("Invalid choice");
 				        break;
