d16cafebbb183a31040c49eb84232559cdf6f6e4
Pravallika Boppana <38985461+pravallika-boppana@users.noreply.github.com>
2020-05-11 17:53:10 +0530

    Added few working methods instead of dummy methods

--- dao/collegeDao.java
+++ dao/collegeDao.java
@@ -1,41 +1,52 @@
 package com.dao;
-import java.util.HashMap;
-import com.user.College;
-import com.user.Preference;
+
 import com.globalStore.Data;
 import com.user.Branch;
+import com.user.College;
+import com.user.Preference;
+import com.user.RequestedAllotment;
 
 public class CollegeDao {
-	public Data addClg = new Data();
+	public Data data = new Data();
 	public void addCollege(College college) {
-		addClg.storeClgDetails(college);
-		System.out.println(addClg.clgDetails.size());
+		Data.colleges.put(college.getClgId(), college);
+	}
+	
+	public College getCollege(String clgId) {
+		return Data.colleges.get(clgId);
 	}
 	
 	public College getThreshold(String clgId) {
-		return addClg.clgDetails.get(clgId);
-		//System.out.println(addClg.clgDetails.get(clgId).branches.toString());
-		//System.out.println(addClg.clgDetails.size());
-	}
-	public void printAllColleges() {
-		addClg.clgDetails.forEach((k,v) -> System.out.println(v.toString()));
-		System.out.println(addClg.clgDetails.size());
+		System.out.println(Data.colleges.containsKey(clgId));
+		return null; }
+	
+	
+
+	public void decrementFilled(Preference preference) {
+		
+		
+		
 	}
 
 	
 
-	public void decrementFilled() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	public College getCollege(Preference pre) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
 	public void incrementFilled(Preference preference) {
+		
+		
+	}
+
+	public College getCollege(Preference preference) {
+		// TODO Auto-generated method stub
+		return Data.colleges.get(preference.getClgId());
+	}
+
+	public void updatePrefenceCount(RequestedAllotment requestedAllotment) {
 		// TODO Auto-generated method stub
 		
+	}
+
+	public Branch getBranch(String clgId, String branchId) {
+		// TODO Auto-generated method stub
+		return Data.colleges.get(clgId).getBranches().get(branchId);
 	}
 }
