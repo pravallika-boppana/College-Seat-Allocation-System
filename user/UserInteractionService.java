c3a75f96d1d32f9850237249e64eb9d861a0c62d
devikasri8 <39004555+devikasri8@users.noreply.github.com>
2020-05-23 12:45:07 +0530

    Update UserInteractionService.java

--- user/UserInteractionService.java
+++ user/UserInteractionService.java
@@ -11,55 +11,22 @@ import com.counselling.service.CounsellingService;
 import com.counselling.service.AllotmentService;
 import com.counselling.user.Student;;
 public class UserInteractionService {
-	
 	public static boolean checkRange(int x) {
 		if (x > 0 && x < 20) 
 			return true;
 		return false;
 	}
-
 	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
-		String line = "";  
-		String splitBy = ",";  
-		try   
-		{  
-		//parsing a CSV file into BufferedReader class constructor  
-		BufferedReader br = new BufferedReader(new FileReader("//home//pravallika//Desktop//counsellingProject//student.csv"));  
-		StudentDao studentDao = new StudentDao();
-		while ((line = br.readLine()) != null)   //returns a Boolean value  
-		{  
-		String[] studentDetails = line.split(splitBy);    // use comma as separator  
-		Student student = new Student(studentDetails[0], Long.parseLong(studentDetails[1]), Long.parseLong(studentDetails[2]));
-		studentDao.addStudent(student);
-		}
-		//s.getAllStudents();
-		line = "";
-		
-		br = new BufferedReader(new FileReader("//home//pravallika//Desktop//counsellingProject//college.csv"));
-		while((line = br.readLine()) != null) {
-		  String[] collegeDetails = line.split(splitBy);
-		  College college = new College(collegeDetails[0], collegeDetails[1], Integer.parseInt(collegeDetails[2]));
-		  for(int i = 3; i < collegeDetails.length; i += 3) {
-			  Branch branch = new Branch(collegeDetails[i], Integer.parseInt(collegeDetails[i+1]), Integer.parseInt(collegeDetails[i+2]));
-			  college.addBranch(branch);
-		   }
-		  CollegeDao collegeDao = new CollegeDao();
-		  collegeDao.addCollege(college);
-		}
-		//totalCollegeDetails.printAllColleges();
-		}
-		catch (IOException e)
-		{  
-		e.printStackTrace();  
-		}  
+		CollegeDao collegeDao = new CollegeDao();
+		CounsellingService counsellingService = new CounsellingService();
+		AllotmentService allotmentService = new AllotmentService();
+		DataLoader dataLoader = new DataLoader();
+		String studentFile = args[0];
+		String collegeFile = args[1];
+		dataLoader.readDataFromFiles(studentFile, collegeFile);
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int choice;
 		int x;
-		CounsellingService counsellingService = new CounsellingService();
-		AllotmentService allotmentService = new AllotmentService();
-		CollegeDao collegeDao = new CollegeDao();
-		StudentDao studentDao = new StudentDao();
 		while(true) {
 			System.out.println("Enter your choice:");
 			System.out.println("1.Request seat allocation");
@@ -68,7 +35,6 @@ public class UserInteractionService {
 			System.out.println("4.Top X unfilled colleges");
 			System.out.println("5.Get Alloted College");
 			System.out.println("6.Get Threshold of the college");
-			System.out.println("7.Get Intake and Filled number of seats  of College");
 			choice = Integer.parseInt(br.readLine());
 			switch(choice) {
 				case 1: System.out.println("Enter Student Id");
@@ -80,7 +46,6 @@ public class UserInteractionService {
 							preferences[i] = new Preference(preference[0], preference[1]);
 						}
 						RequestedAllotment requestAllotment = counsellingService.addPreferences(stuId, preferences);
-						
 						allotmentService.allotClg(requestAllotment);
 						break;
 				case 2:System.out.println("How many top desirable colleges you want?(select a number < 20)");
@@ -117,11 +82,7 @@ public class UserInteractionService {
 				case 6:System.out.println("Enter college id");
 						String clgId = br.readLine();
 						collegeDao.getThreshold(clgId);
-						break;
-				case 7:System.out.println("Enter College ID : ");
-				       String clgId2 = br.readLine();
-				       collegeDao.getIntakeAndFilled(clgId2);
-				       break;
+						break;	
 				default:System.out.println("Invalid choice");
 				        break;
 			}
@@ -130,5 +91,4 @@ public class UserInteractionService {
 				break;
 		}
 	}
-
 }
