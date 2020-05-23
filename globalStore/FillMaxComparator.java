7afc6e810cdbcfa87ea2d827da8292d7966cb1c8
ApoorvaNamala <38985566+ApoorvaNamala@users.noreply.github.com>
2020-05-23 20:43:33 +0530

    Updated

--- globalStore/FillMaxComparator.java
+++ globalStore/FillMaxComparator.java
@@ -6,10 +6,10 @@ import com.counselling.user.College;
 
 public class FillMaxComparator implements Comparator<College>{
 	public int compare(College c1, College c2){
-        if(c1.getTotalPref() < c2.getTotalPref()){
+        if(c1.getFilled() < c2.getFilled()){
             return 1;
         }
-        if(c1.getTotalPref() > c2.getTotalPref()){
+        if(c1.getFilled() > c2.getFilled()){
             return -1;
         }
         return 0;
