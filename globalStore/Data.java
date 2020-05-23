e07a71a37a41121272a6deca3a161bc3c684056c
ApoorvaNamala <38985566+ApoorvaNamala@users.noreply.github.com>
2020-05-23 20:51:51 +0530

    Update Data.java

--- globalStore/Data.java
+++ globalStore/Data.java
@@ -7,6 +7,6 @@ import com.counselling.user.*;
 public class Data {
 	public static HashMap<String, College> colleges = new HashMap<String, College>();
 	public static Trie students = new Trie();
-	public static PriorityQueue<College> topXDesirable = new PriorityQueue<>(new PrefMaxComparator());	
-	public static PriorityQueue<College> topXFilled = new PriorityQueue<>(new FillMaxComparator());	
-	public static PriorityQueue<College> topXunFilled = new PriorityQueue<>(new UnfillMinComparator());	 }
+	public static  MaxHeap topDesirable = new MaxHeap();
+	public static  MaxHeap topFilled = new MaxHeap();
+	public static  MinHeap topUnfilled = new MinHeap();	 }
