6892f70dc0402f65b16b44d300b36acd2fca43a0
devikasri8 <39004555+devikasri8@users.noreply.github.com>
2020-05-24 15:49:37 +0530

    Update UserInteractionService.java

--- service/UserInteractionService.java
+++ service/UserInteractionService.java
@@ -9,22 +9,78 @@ import com.counselling.user.Preference;
 import com.counselling.user.RequestedAllotment;
 import com.counselling.service.AllotmentService;;
 public class UserInteractionService {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static CollegeDao collegeDao = new CollegeDao();
+	static CounsellingService counsellingService = new CounsellingService();
+	static AllotmentService allotmentService = new AllotmentService();
+	
 	public static boolean checkRange(int x) {
 		if (x > 0 && x < 20) 
 			return true;
 		return false;
 	}
+	
+	public static void processRequest(int choice) throws  IOException {
+		int x;
+		switch(choice) {
+			case 1: System.out.println("Enter Student Id");
+					long stuId = Long.parseLong(br.readLine());
+					System.out.println("Enter Student preferences as college Id and Branch Id");
+					Preference[] preferences = new Preference[3];
+					for(int i = 0; i < 3; i++) {
+						String[] preference = br.readLine().split(" ");
+						preferences[i] = new Preference(preference[0], preference[1]);
+					}
+					RequestedAllotment requestAllotment = counsellingService.addPreferences(stuId, preferences);
+					allotmentService.allotClg(requestAllotment);
+					break;
+			case 2:System.out.println("How many top desirable colleges you want?(select a number < 20)");
+					x = Integer.parseInt(br.readLine());
+					if(checkRange(x)) {						
+						    System.out.println("Top " + x +" Desirable colleges");
+					    counsellingService.topDesirable(x);
+					}
+					else
+						System.out.println("Invalid number");
+					break;
+			case 3:System.out.println("How many top filled colleges you want?(select a number < 20)");
+					x = Integer.parseInt(br.readLine());
+					if(checkRange(x)) {
+						System.out.println("Top " + x +" filled colleges");
+						counsellingService.topFilled(x);
+					}
+					else
+						System.out.println("Invalid number");
+					break;
+			case 4:System.out.println("How many top unfilled colleges you want?(select a number < 20)");
+					x = Integer.parseInt(br.readLine());
+					if(checkRange(x)) {
+						System.out.println("Top " + x +" unfilled colleges");
+						counsellingService.topUnfilled(x);
+					}
+					else
+						System.out.println("Invalid number");
+					break;
+			case 5:System.out.println("Enter student Id");
+					long stdId = Long.parseLong(br.readLine());
+					System.out.println(counsellingService.getAllotedCollege(stdId));
+					break;
+			case 6:System.out.println("Enter college id");
+					String clgId = br.readLine();
+					collegeDao.getThreshold(clgId);
+					break;	
+			default:System.out.println("Invalid choice");
+			        break;
+		}
+	}
+
 	public static void main(String[] args) throws IOException {
-		CollegeDao collegeDao = CollegeDao.getInstance();
-		CounsellingService counsellingService = new CounsellingService();
-		AllotmentService allotmentService = new AllotmentService();
+		
 		DataLoader dataLoader = new DataLoader();
 		String studentFile = args[0];
 		String collegeFile = args[1];
 		dataLoader.readDataFromFiles(studentFile, collegeFile);
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int choice;
-		int x;
 		while(true) {
 			System.out.println("Enter your choice:");
 			System.out.println("1.Request seat allocation");
@@ -34,58 +90,12 @@ public class UserInteractionService {
 			System.out.println("5.Get Alloted College");
 			System.out.println("6.Get Threshold of the college");
 			choice = Integer.parseInt(br.readLine());
-			switch(choice) {
-				case 1: System.out.println("Enter Student Id");
-						long stuId = Long.parseLong(br.readLine());
-						System.out.println("Enter Student preferences as college Id and Branch Id");
-						Preference[] preferences = new Preference[3];
-						for(int i = 0; i < 3; i++) {
-							String[] preference = br.readLine().split(" ");
-							preferences[i] = new Preference(preference[0], preference[1]);
-						}
-						RequestedAllotment requestAllotment = counsellingService.addPreferences(stuId, preferences);
-						allotmentService.allotClg(requestAllotment);
-						break;
-				case 2:System.out.println("How many top desirable colleges you want?(select a number < 20)");
-						x = Integer.parseInt(br.readLine());
-						if(checkRange(x)) {						
-     					    System.out.println("Top " + x +" Desirable colleges");
-						    QueryingService.topDesirable(x);
-						}
-						else
-							System.out.println("Invalid number");
-						break;
-				case 3:System.out.println("How many top filled colleges you want?(select a number < 20)");
-						x = Integer.parseInt(br.readLine());
-						if(checkRange(x)) {
-							System.out.println("Top " + x +" filled colleges");
-							QueryingService.topFilled(x);
-						}
-						else
-							System.out.println("Invalid number");
-						break;
-				case 4:System.out.println("How many top unfilled colleges you want?(select a number < 20)");
-						x = Integer.parseInt(br.readLine());
-						if(checkRange(x)) {
-							System.out.println("Top " + x +" unfilled colleges");
-							QueryingService.topUnfilled(x);
-						}
-						else
-							System.out.println("Invalid number");
-						break;
-				case 5:System.out.println("Enter student Id");
-						long stdId = Long.parseLong(br.readLine());
-						System.out.println(counsellingService.getAllotedCollege(stdId));
-						break;
-				case 6:System.out.println("Enter college id");
-						String clgId = br.readLine();
-						collegeDao.getThreshold(clgId);
-						break;	
-				default:System.out.println("Invalid choice");
-				        break;
-			}
-			System.out.println("Do you want to continue(YES / NO)");
-			if(br.readLine().equals("NO"))
+			processRequest(choice);
+			System.out.println("Hit 'Y' to countinue or hit any key to exit");
+			if(br.readLine().equals("Y"))
+				continue;
+			else
+				System.out.println("Exited");
 				break;
 		}
 	}
