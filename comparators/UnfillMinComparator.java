69e9cc1d95bb8935c3ba6fbd2594b8da45509e38
ApoorvaNamala <38985566+ApoorvaNamala@users.noreply.github.com>
2020-05-24 23:30:34 +0530

    Updated

--- comparators/UnfillMinComparator.java
+++ comparators/UnfillMinComparator.java
@@ -1,16 +1,15 @@
-package com.counselling.comparators;
+package com.counselling.globalStore;
+
 import java.util.Comparator;
 
 import com.counselling.user.College;
 
-public class UnfillMinComparator implements Comparator<College>{
+public class FillMaxComparator implements Comparator<College>{
 	public int compare(College c1, College c2){
-		int unfilled1 = c1.getTotalIntake() - c1.getFilled();
-		int unfilled2 = c2.getTotalIntake() - c2.getFilled();
-        if(unfilled1 < unfilled2){
+        if(c1.getUnfilledPercent() < c2.getUnfilledPercent()){
             return 1;
         }
-        if(unfilled1 > unfilled2){
+        if(c1.getUnfilledPercent() > c2.getUnfilledPercent()){
             return -1;
         }
         return 0;
