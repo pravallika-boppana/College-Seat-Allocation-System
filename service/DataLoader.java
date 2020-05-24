5f3bc8ac3baf7976fbe1c34670a917540b086f42
devikasri8 <39004555+devikasri8@users.noreply.github.com>
2020-05-24 20:09:45 +0530

    Update DataLoader.java

--- service/DataLoader.java
+++ service/DataLoader.java
@@ -11,8 +11,23 @@ import com.counselling.user.Student;
 import java.util.StringTokenizer;
 
 public class DataLoader {
-	static CollegeDao collegeDao = CollegeDao.getInstance();
-	static StudentDao studentDao = StudentDao.getInstance();
+	CollegeDao collegeDao = new CollegeDao();
+	StudentDao studentDao = new StudentDao();
+
+	public boolean isFileExists(String studentFile, String collegeFile) {
+		if(new File(studentFile).exists() && new File(collegeFile).exists()) 
+			return true;
+		else { 
+			if((!new File(studentFile).exists()) && (!new File(collegeFile).exists()))
+				System.out.println("Both the given files not exists");
+			else if(!new File(studentFile).exists())
+				System.out.println("Given student file not exists");
+			else if(!new File(collegeFile).exists())
+				System.out.println("Given college file not exists");
+			return false;
+		}	
+	}
+
 	public void readDataFromFiles(String studentFile, String collegeFile) {
 		String line = "";  
 		try   
@@ -20,44 +35,54 @@ public class DataLoader {
 			//parsing a CSV file into BufferedReader class constructor 
 			BufferedReader br = new BufferedReader(new FileReader(studentFile));  
 			StringTokenizer studentDetails = null;
-			while ((line = br.readLine()) != null)   //returns a Boolean value  
+			while ((line = br.readLine()) != null)   
 			{  
 				studentDetails = new StringTokenizer(line, ",");
-				//String studentDetails = line.split(splitBy);    // use comma as separator 
-				String stuName = studentDetails.nextToken();
-				long stuId = Long.parseLong(studentDetails.nextToken());
-				long rank = Long.parseLong(studentDetails.nextToken());
-				Student student = new Student(stuName,stuId ,rank );
-				studentDao.addStudent(student);
-			
+				try {
+					String stuName = studentDetails.nextToken();
+					long stuId = Long.parseLong(studentDetails.nextToken());
+					long rank = Long.parseLong(studentDetails.nextToken());
+					if((stuName.matches("^[ a-zA-Z]*$")) && (stuId == (int)stuId) && (rank == (int)rank)) {
+						Student student = new Student(stuName,stuId ,rank );
+						studentDao.addStudent(student);
+					}
+				}
+				catch(NumberFormatException e) {
+					continue;
+				}
 			}
-			//s.getAllStudents();
 			line = "";
 			StringTokenizer collegeDetails = null;
 			br = new BufferedReader(new FileReader(collegeFile));
 			while((line = br.readLine()) != null) {
 				collegeDetails = new StringTokenizer(line, ",");
-				String clgName = collegeDetails.nextToken();
-				String clgId = collegeDetails.nextToken();
-				int totalIntake = Integer.parseInt(collegeDetails.nextToken());
-				College college = new College(clgName, clgId, totalIntake);
-				while(collegeDetails.hasMoreTokens())
-                {
-					String branchId = collegeDetails.nextToken();
-					int threshold = Integer.parseInt(collegeDetails.nextToken());
-					int intake =  Integer.parseInt(collegeDetails.nextToken());
-					Branch branch = new Branch(branchId, threshold, intake);
-					college.addBranch(branch);
-				}  
-				collegeDao.addCollege(college);
+				try {
+					String clgName = collegeDetails.nextToken();
+					String clgId = collegeDetails.nextToken();
+					int totalIntake = Integer.parseInt(collegeDetails.nextToken());
+					if(clgName.matches("^[ a-zA-Z]*$") && clgId.matches("^[a-zA-Z]*$") && totalIntake == (int)totalIntake) {
+						College college = new College(clgName, clgId, totalIntake);
+						while(collegeDetails.hasMoreTokens())
+						{
+							String branchId = collegeDetails.nextToken();
+							int threshold = Integer.parseInt(collegeDetails.nextToken());
+							int intake =  Integer.parseInt(collegeDetails.nextToken());
+							if(branchId.matches("^[ a-zA-Z]*$") && threshold == (int)threshold && intake == (int)intake) {
+								Branch branch = new Branch(branchId, threshold, intake);
+								college.addBranch(branch);
+							}
+						}  
+						collegeDao.addCollege(college);
+					}
+				}
+				catch(NumberFormatException e) {
+					continue;
+				}
 			}
-			
-			//totalCollegeDetails.printAllColleges();
 		}
 		catch (IOException e)
 		{  
-			e.printStackTrace();  
+			e.printStackTrace();
 		} 
 	}
 }
- 
