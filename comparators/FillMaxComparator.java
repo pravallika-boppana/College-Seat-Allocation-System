6614813e6479f9d03edf00f37fad03ac179f4f8e
ApoorvaNamala <38985566+ApoorvaNamala@users.noreply.github.com>
2020-05-24 23:25:40 +0530

    Updated

--- comparators/FillMaxComparator.java
+++ comparators/FillMaxComparator.java
@@ -1,4 +1,4 @@
-package com.counselling.comparators;
+package com.counselling.globalStore;
 
 import java.util.Comparator;
 
@@ -6,10 +6,10 @@ import com.counselling.user.College;
 
 public class FillMaxComparator implements Comparator<College>{
 	public int compare(College c1, College c2){
-        if(c1.getFilled() < c2.getFilled()){
+        if(c1.getFilledPercent() < c2.getFilledPercent()){
             return 1;
         }
-        if(c1.getFilled() > c2.getFilled()){
+        if(c1.getFilledPercent() > c2.getFilledPercent()){
             return -1;
         }
         return 0;
