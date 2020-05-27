5963a0eaffe1f2666b06b74696a642cabcc68c24
ApoorvaNamala <38985566+ApoorvaNamala@users.noreply.github.com>
2020-05-27 20:13:14 +0530

    Updated

--- comparators/PrefMaxComparator.java
+++ comparators/PrefMaxComparator.java
@@ -4,7 +4,7 @@ import java.util.Comparator;
 
 import com.counselling.user.College;
 
-public class FillMaxComparator implements Comparator<College>{
+public class PrefMaxComparator implements Comparator<College>{
 	public int compare(College c1, College c2){
         if(c1.getPreferredPercent() < c2.getPreferredPercent()){
             return 1;
